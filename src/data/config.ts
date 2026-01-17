4e023babc14178a51e47ef87766ee76c88441f09
Chandan Kumar Dalai <kumardalaichandan803@gmail.com>
2026-01-17 21:08:29 +0530

    cleanup lock files and update portfolio content

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,9 +1,9 @@
 const config = {
-  title: "Naresh Khatri | Full-Stack Developer",
+  title: "Deepak | Full-Stack Developer",
   description: {
-    long: "Explore the portfolio of Naresh, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
+    long: "Explore the portfolio of Deepak, a full-stack developer and creative technologist specializing in interactive web experiences, 3D animations, and innovative projects. Discover my latest work, including Coding Ducks, The Booking Desk, Ghostchat, and more. Let's build something amazing together!",
     short:
-      "Discover the portfolio of Naresh, a full-stack developer creating interactive web experiences and innovative projects.",
+      "Discover the portfolio of Deepak, a full-stack developer creating interactive web experiences and innovative projects.",
   },
   keywords: [
     "Naresh",
@@ -23,23 +23,23 @@ const config = {
     "Spline",
     "Framer Motion",
   ],
-  author: "Naresh Khatri",
-  email: "naresh.khatri2345@gmail.com",
-  site: "https://nareshkhatri.site",
+  author: "Deepak",
+  email: "deepak.ghoda2345@gmail.com",
+  site: "https://deepak.site",
 
   // for github stars button
-  githubUsername: "naresh-khatri",
+  githubUsername: "chandan0000001",
   githubRepo: "3d-portfolio",
 
   get ogImg() {
     return this.site + "/assets/seo/og-image.png";
   },
   social: {
-    twitter: "https://x.com/nothotchaddi",
-    linkedin: "https://www.linkedin.com/in/naresh-khatri/",
-    instagram: "https://www.instagram.com/hotchaddi",
-    facebook: "https://www.facebook.com/HotChaddi/",
-    github: "https://github.com/Naresh-Khatri",
+    twitter: "https://x.com/DeepakKuma97056",
+    linkedin: "https://www.linkedin.com/in/chandan-kumar-dalai/",
+    instagram: "https://www.instagram.com/abcd",
+    facebook: "https://www.facebook.com/abcd/",
+    github: "https://github.com/chandan0000001",
   },
 };
 export { config };
